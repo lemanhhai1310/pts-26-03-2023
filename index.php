dccba4276cb59884e04189516ae65b72102bec0f
hailm <hai.lemanh.1310@gmail.com>
2023-03-26 15:24:35 +0700

    feat: init project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php $data["title"] = "Home"; ?>
+<?php $bodyClass = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-position-relative uk-background-norepeat uk-background-center-center uk-background-cover" data-src="images/bg1.png" uk-img>
+    <div class="uk-overlay uk-overlay-primary uk-position-cover"></div>
+    <div class="uk-position-relative uk-section-small uk-position-z-index">
+        <div class="uk-container uk-container-large">
+            <div class="uk-card uk-card-default uk-card-body uk-height-min-large uk-border-rounded">
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
